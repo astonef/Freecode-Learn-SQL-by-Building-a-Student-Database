ad9c087d2824dd89c00579f1df3c78d924a1940a
Stefano <stefano.g.magni@hotmail.it>
2024-02-17 09:40:12 +0100

    Raggiunto 840 da fare

--- insert_data.sh
+++ insert_data.sh
@@ -1,20 +1,37 @@
-#!/bin/bash 
-# Sono a 568 da fare
-
-cat courses.csv | while IFS=',' read MAJOR COURSE
-do
+#!/bin/bash
+#Sono all'840 da fare
 
 PSQL="psql -X --username=postgres --dbname=students --no-align --tuples-only -c"
 
-  # get major_id
+cat courses_test.csv | while IFS=',' read MAJOR COURSE
+
+do
+
+if [[ $MAJOR != major ]] 
+
+then	
+
+# get major_id
   MAJOR_ID=$($PSQL "SELECT major_id FROM majors WHERE major='$MAJOR';")
-echo $MAJOR_ID 
+
 
 # if not found
+if [[ -z $MAJOR_ID ]]
+then
 
   # insert major
+  INSERT_MAJOR_RESULT=$($PSQL "INSERT INTO majors(major) VALUES('$MAJOR')")
 
+  if [[ $INSERT_MAJOR_RESULT == "INSERT 0 1" ]]
+  then
+	 echo "Inserted into majors, $MAJOR"
+  fi 
+
+  
   # get new major_id
+
+  MAJOR_ID=$(PSQL "SELECT major_id FROM majors WHERE major='$MAJOR'")
+fi
 
   # get course_id
 
@@ -25,7 +42,7 @@ echo $MAJOR_ID
   # get new course_id
 
   # insert into majors_courses
-
+fi
 done	
 
 
